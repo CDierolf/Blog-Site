361f45a2a346d67752dc0f644361c28b4a1b1249
Christopher Dierolf <chidi117@gmail.com>
2019-02-19 15:03:06 -0600

    initial commit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const lodash = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -11,15 +12,32 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
-const posts = [];
-
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb+srv://Admin-C:test123@cluster0-jnq1u.mongodb.net/blogDB", {useNewUrlParser: true});
+
+// SCHEMA's
+const postSchema = new mongoose.Schema({
+  postTitle: {
+    type: String,
+    required: true
+  },
+  postBody: {
+    type: String,
+    required: true
+  }
+});
+
+const Post = mongoose.model("Post", postSchema);
+
 app.get('/', function(req, res) {
-  res.render('home', {homeStartingContent: homeStartingContent, posts: posts});
+  Post.find({}, function(err, posts) {
+    if (!err) {
+      res.render('home', {homeStartingContent: homeStartingContent, posts: posts});
+    }
+  });
 });
 
 app.get('/about', function(req,res) {
@@ -34,43 +52,24 @@ app.get('/compose', function(req, res) {
   res.render('compose');
 });
 
-app.get('/post/:postTitle', function(req,res) {
-  let title = lodash.lowerCase(req.params.postTitle);
-  let body;
-  let found = false;
-  for (i = 0; i < posts.length; i++) {
-    if (lodash.lowerCase(posts[i].title) === title) {
-      body = posts[i].body;
-      found = true;
-      break;
-    }
-  }
-  if (found == true) {
-    res.render('post', {postTitle: title, postBody: body});
-  }
-  
+app.get('/posts/:postID', function(req,res) {
+
+  let id = req.params.postID;
+  // get title and body based on
+  Post.findById(id, function(e, results) {
+    res.render('post', {postTitle: results.postTitle, postBody: results.postBody});
+  });
 });
 
 app.post('/compose', function(req, res) {
-  let post = {
-    title: req.body.postTitle,
-    body: req.body.postBody
-  };
-  posts.push(post);
+
+  const post = new Post({
+    postTitle: req.body.postTitle,
+    postBody: req.body.postBody
+  });
+    post.save();
   res.redirect("/");
 });
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
