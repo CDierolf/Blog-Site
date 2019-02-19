03f9164aaa2941738cd4bce8d0fd844f17928ac4
Christopher Dierolf <chidi117@gmail.com>
2019-02-19 15:12:52 -0600

    added procfile and gitignore

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//jshint esversion:6
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -12,10 +11,14 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+
+
+// App sets/uses
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+// MongoDB Connction
 mongoose.connect("mongodb+srv://Admin-C:test123@cluster0-jnq1u.mongodb.net/blogDB", {useNewUrlParser: true});
 
 // SCHEMA's
@@ -70,7 +73,12 @@ app.post('/compose', function(req, res) {
     post.save();
   res.redirect("/");
 });
+// Port
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
